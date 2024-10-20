85c47aec9967c415228772287502fc3b5b8135ba
Veer.Shah <shahveer1213@gmail.com>
2024-10-20 13:22:32 -0700

    Add project files.

---
+++ MergeSort/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MergeSort
+{
+    internal class Program
+    {
+        public static void MergeSort()
+        {
+
+        }
+
+        public static void Merge()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
