5fc9c597d9a9a269c403716f7d67b70c9f23ba44
Veer.Shah <shahveer1213@gmail.com>
2024-10-20 15:01:32 -0700

    finished making the merge but there are many mistakes

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -2,12 +2,12 @@
 {
     internal class Program
     {
-        public static void MergeSort<T>(List<T> list)
+        public static List<T> MergeSort<T>(List<T> list)
+            where T : IComparable<T>
         {
             if (list.Count < 2)
             {
-                Console.WriteLine(list[0]);
-                return;
+                return list;
             }
 
 
@@ -24,24 +24,65 @@
                 list2.Add(list[i]);
             }
 
+
             MergeSort(list1);
             MergeSort(list2);
 
-            
+
+            Merge(list, list1, list2);
+
+            return list;
+
 
        
 
         }
 
-        public static void Merge()
+        public static void Merge<T>(List<T> originalList, List<T> list1, List<T> list2)
+            where T : IComparable<T>
         {
 
+            for (int c = 0, i = 0, j = 0; c < originalList.Count;)
+            {
+                if(i >= list1.Count)
+                {
+                    for(j = j; j < list2.Count; j++)
+                    {
+                        originalList.Add(list2[j]);
+                    }
+                    break;
+                }
+                else if(j >= list2.Count)
+                {
+                    for(i = i; i < list1.Count; i++)
+                    {
+                        originalList.Add(list1[i]);
+                    }
+                    break;
+                }
+                if (list1[i].CompareTo(list2[j]) < 0) 
+                {
+                    originalList[c] = list1[i];
+                    i++;
+                }
+                else if(j < list2.Count)
+                {
+                    originalList[c] = list2[j];
+                    j++;
+                }
+                c++;
+            }
         }
         static void Main(string[] args)
         {
             List<int> list = new List<int>(new int[] { 10, 1, 2, 4, 3, 6, 5, 7, 8, 9 });
             
-            MergeSort(list);
+            list = MergeSort(list);
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
             
             
         }
